981018bd2987485d6b899f4bfe135d9df04f4a74
Guilherme Cardoso <guilherme.martins@itixti.com.br>
2025-12-06 15:04:30 -0300

    Testando versionamento de código no Netbeans

--- src/main/java/com/ufes/sistema/Main.java
+++ src/main/java/com/ufes/sistema/Main.java
@@ -11,7 +11,6 @@ package com.ufes.sistema;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        
+        System.out.println("Testando git no Netbeans");
     }
 }
